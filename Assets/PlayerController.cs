7a75c5bf17fc08197c6e78d063e764d25f8ccda9
Caleb Belrose <calebbelrose@gmail.com>
2016-11-08 20:07:32 -0500

    Made it so the putter actually hits the ball, added text for power level and stroke count, changed position of putter and camera after shots

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,22 +4,87 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
-
     private Rigidbody rb;
+    public Vector3 startingPosition;
+    float minX, maxX, minZ, maxZ;
+    float xDistance, zDistance;
+    bool newMax = false;
+    GUI score;
+    double power = 0;
+    GUIStyle guiStyle;
+    int strokes = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        guiStyle = new GUIStyle();
+        guiStyle.fontSize = 30;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, Screen.height - 30, 100, 30), "Power: " + power.ToString("0.00"), guiStyle);
+        GUI.Label(new Rect(0, Screen.height - 60, 100, 30), "Strokes: " + strokes, guiStyle);
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        double x, z;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (moveVertical != 0f)
+        {
+            transform.position = new Vector3(transform.position.x + moveVertical * xDistance, transform.position.y, transform.position.z + moveVertical * zDistance);
+        }
 
-        rb.AddForce(movement * speed);
+        if (transform.position.x > maxX)
+        {
+            if (newMax)
+            {
+                rb.Sleep();
+                gameObject.SetActive(false);
+            }
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < minX)
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+
+        if (transform.position.z > maxZ)
+        {
+            if (newMax)
+            {
+                rb.Sleep();
+                gameObject.SetActive(false);
+            }
+            transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+        }
+        else if (transform.position.z < minZ)
+            transform.position = new Vector3(transform.position.x, transform.position.y, minZ);
+
+        x = (double)(transform.position.x - startingPosition.x);
+        z = (double)(transform.position.z - startingPosition.z);
+        power = System.Math.Sqrt(x * x + z * z) * 25;
+
+        if (Input.GetKeyDown("space") && !newMax)
+        {
+            newMax = true;
+            maxX = startingPosition.x + (startingPosition.x - transform.position.x) * xDistance;
+            maxZ = startingPosition.z + (startingPosition.z - transform.position.z) * zDistance;
+            Vector3 movement = new Vector3(startingPosition.x - transform.position.x, 0.0f, startingPosition.z - transform.position.z);
+            rb.AddForce(movement * 1000);
+            strokes++;
+        }
+    }
+
+    public void SetClamps(float xD, float zD)
+    {
+        newMax = false;
+        minX = transform.position.x - 4f * xD;
+        maxX = startingPosition.x;
+        minZ = transform.position.z - 4f * zD;
+        maxZ = startingPosition.z;
+        xDistance = xD;
+        zDistance = zD;
     }
 }
---
+++ Assets/ResetPutter.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPutter : MonoBehaviour
+{
+
+    public GameObject putter, ballObject, camera;
+    private GameObject hole, wall;
+    private Rigidbody rb;
+    int holeNumber = 0;
+    private Vector3 cameraOffset, prevPos;
+    bool needReset = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        NewHole();
+        cameraOffset = camera.transform.position - transform.position;
+        prevPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != prevPos)
+        {
+            camera.transform.position = transform.position + cameraOffset;
+            needReset = true;
+        }
+        else if (rb.IsSleeping() && needReset)
+        {
+            needReset = false;
+            float numerator1, numerator2, denominator, xDistance, zDistance;
+            PlayerController pc;
+
+            putter.SetActive(true);
+            Physics.IgnoreCollision(wall.GetComponent<Collider>(), putter.GetComponent<Collider>());
+            ballObject.transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+            numerator1 = hole.transform.position.x - transform.position.x;
+            numerator2 = hole.transform.position.z - transform.position.z;
+            denominator = Mathf.Abs((float)System.Math.Sqrt(numerator1 * numerator1 + numerator2 * numerator2));
+            xDistance = numerator1 / denominator;
+            zDistance = numerator2 / denominator;
+            putter.transform.position = new Vector3(transform.position.x - 0.5f * xDistance, ballObject.transform.position.y, transform.position.z - 0.5f * zDistance);
+            camera.transform.position = new Vector3(transform.position.x - 35.5f * xDistance, ballObject.transform.position.y + 20f, transform.position.z - 35.5f * zDistance);
+            putter.transform.LookAt(ballObject.transform);
+            camera.transform.LookAt(ballObject.transform);
+            camera.transform.Rotate(-19.378f, 0f, 0f);
+            pc = putter.GetComponent<PlayerController>();
+            pc.startingPosition = putter.transform.position;
+            pc.SetClamps(xDistance, zDistance);
+        }
+        prevPos = transform.position;
+    }
+
+    public void NewHole()
+    {
+        holeNumber++;
+        hole = GameObject.Find("hole" + holeNumber);
+        wall = GameObject.Find("wall" + holeNumber);
+        Physics.IgnoreCollision(wall.GetComponent<Collider>(), putter.GetComponent<Collider>());
+    }
+}
